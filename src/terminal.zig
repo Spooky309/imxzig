2458dc194dea57146a353a6d129b7a6f932b1b4e
Spooky309 <admin@koles.tech>
2025-11-23 15:43:44 +0000

    Slop changes because I don't fancy doing any more real work today

--- src/terminal.zig
+++ src/terminal.zig
@@ -37,6 +37,7 @@ fn doCommand() !void {
         }
     }
     try putCommandPrompt();
+    try stdioWriter.flush();
     terminalBufferTop = 0;
 }
 
@@ -54,7 +55,8 @@ fn receiveChars() !void {
             terminalBuffer[terminalBufferTop] = c;
             terminalBufferTop += 1;
             // echo it back to them
-            _ = try stdioWriter.write(&b);
+            _ = try stdioWriter.writeAll(&b);
+            try stdioWriter.flush();
         }
     }
     lastCharReceived = c;
@@ -70,44 +72,145 @@ fn helpCommand(_: []const []const u8) void {
     _ = stdioWriter.write("There is no help yet.\n") catch {};
 }
 
-fn testWait(ms: u32) !void {
-    const before = client.readPerformanceCounter();
-    client.sleep(ms);
-    const now = client.readPerformanceCounter();
-    const delta = if (now >= before) now - before else now + (0xFFFFFFFF - before);
-    const deltaMs = delta / (client.getPerformanceCounterFrequencyHz() / 1_000);
+// Fields of the struct in the array should be scalars. Any \n or \t in the format output will break it...
+fn writeArrayAsTable(arr: anytype) !void {
+    const rowTypeInfo = @typeInfo(@typeInfo(@TypeOf(arr)).array.child).@"struct";
+    var columnWidths: [rowTypeInfo.fields.len]u32 = @splat(0);
+    inline for (rowTypeInfo.fields, 0..) |columnField, columnIndex| {
+        columnWidths[columnIndex] = columnField.name.len + 2; // +2 for a space on either side...
+        for (arr) |rowValue| {
+            const fieldValue = @field(rowValue, columnField.name);
+            columnWidths[columnIndex] = @max(columnWidths[columnIndex], std.fmt.count("{}", .{fieldValue}) + 2);
+        }
+    }
 
-    try stdioWriter.print("Expected:\t{}ms\tActual: {}ms\n", .{ ms, deltaMs });
+    for (columnWidths) |w| {
+        // +1 for the | on either side of a column
+        for (0..w + 1) |_| {
+            _ = try stdioWriter.write("-");
+        }
+    }
+
+    _ = try stdioWriter.write("-\n");
+
+    inline for (rowTypeInfo.fields, 0..) |columnField, columnIndex| {
+        // Print column separator and a space for the value
+        _ = try stdioWriter.write("| ");
+        // Figure out how long the value is, +1 for the space we just added.
+        const amtWritten = columnField.name.len + 1;
+        // Print the name of the field
+        _ = try stdioWriter.write(columnField.name);
+        // pad with spaces
+        for (amtWritten..columnWidths[columnIndex]) |_| {
+            _ = try stdioWriter.write(" ");
+        }
+    }
+
+    _ = try stdioWriter.write("|\n");
+
+    for (columnWidths) |w| {
+        // +1 for the | on either side of a column
+        for (0..w + 1) |_| {
+            _ = try stdioWriter.write("-");
+        }
+    }
+
+    _ = try stdioWriter.write("-\n");
+
+    for (arr) |rowValue| {
+        inline for (rowTypeInfo.fields, 0..) |columnField, columnIndex| {
+            // Print column separator and a space for the value
+            _ = try stdioWriter.write("| ");
+            // Figure out how long the value is, +1 for the space we just added.
+            const amtWritten = std.fmt.count("{}", .{@field(rowValue, columnField.name)}) + 1;
+            // Print the value itself
+            try stdioWriter.print("{}", .{@field(rowValue, columnField.name)});
+            // pad with spaces
+            for (amtWritten..columnWidths[columnIndex]) |_| {
+                _ = try stdioWriter.write(" ");
+            }
+        }
+        // End the row
+        _ = try stdioWriter.write("|\n");
+    }
+
+    for (columnWidths) |w| {
+        // +2 for the | on either side of a column
+        for (0..w + 1) |_| {
+            _ = try stdioWriter.write("-");
+        }
+    }
+
+    _ = try stdioWriter.write("-\n");
+
+    try stdioWriter.flush();
 }
 
 const splatBytes: [4096]u8 = @splat('A');
 fn testSplatCommand(_: []const []const u8) void {
+    stdioWriter.flush() catch {};
+
     const before = client.readPerformanceCounter();
-    _ = stdioWriter.write(&splatBytes) catch {};
+
+    // Call write directly to bypass writer overhead
+    _ = client.write(0, &splatBytes);
+
     const now = client.readPerformanceCounter();
     const delta = if (now >= before) now - before else now + (0xFFFFFFFF - before);
     const deltaMs = delta / (client.getPerformanceCounterFrequencyHz() / 1_000);
     stdioWriter.print("\nThat was {} bytes. It took us {}ms (from process perspective)\n", .{ splatBytes.len, deltaMs }) catch {};
 }
 
-fn testWaitCommand(_: []const []const u8) void {
-    _ = stdioWriter.write("Testing wait times\n") catch {};
-    testWait(1) catch {};
-    testWait(5) catch {};
-    testWait(10) catch {};
-    testWait(100) catch {};
-    testWait(500) catch {};
-    testWait(1000) catch {};
+const TestWaitResult = struct {
+    expectedMs: u32 = 0,
+    actualMs: u32 = 0,
+    successful: bool = false,
+};
+
+fn testWait(ms: u32) !TestWaitResult {
+    _ = try stdioWriter.print("Testing wait of {}ms\n", .{ms});
+    try stdioWriter.flush();
+
+    const before = client.readPerformanceCounter();
+    client.sleep(ms);
+    const now = client.readPerformanceCounter();
+    const delta = if (now >= before) now - before else now + (0xFFFFFFFF - before);
+    const deltaMs = delta / (client.getPerformanceCounterFrequencyHz() / 1_000);
+
+    return .{
+        .expectedMs = ms,
+        .actualMs = deltaMs,
+        .successful = true,
+    };
 }
 
+fn testWaitCommand(_: []const []const u8) void {
+    _ = stdioWriter.write("Please wait...\n") catch {};
+
+    const results: [6]TestWaitResult = .{
+        testWait(1) catch .{},
+        testWait(5) catch .{},
+        testWait(10) catch .{},
+        testWait(100) catch .{},
+        testWait(500) catch .{},
+        testWait(1000) catch .{},
+    };
+
+    writeArrayAsTable(results) catch {};
+}
+
+var stdioWriterBuffer: [1024]u8 = undefined;
 pub fn task() !void {
     stdioWriter = client.stdioWriter();
+    stdioWriter.buffer = &stdioWriterBuffer;
 
     try registerCommand("testSplat", testSplatCommand);
     try registerCommand("testWait", testWaitCommand);
     try registerCommand("help", helpCommand);
 
     try putCommandPrompt();
+    try stdioWriter.flush();
+
     while (true) {
         try receiveChars();
     }
